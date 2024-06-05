e5c11e9cd09c3a1cb542fc3bfda7d6d9e90cee94
Matt Sturgeon <matt@sturgeon.me.uk>
2024-06-05 03:14:55 +0100

    nixvim: simplify enable conditions
    Checking `config.programs ? nixvim` **and** `options.programs ? nixvim`
    is redundant; the `mkIf` check had no benefit.
    
    This now matches the pattern used elsewhere.

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -13,46 +13,44 @@
     };
   };
 
-  config = lib.mkIf ((config.programs ? nixvim) && config.stylix.targets.nixvim.enable) (
-    lib.optionalAttrs (builtins.hasAttr "nixvim" options.programs) {
-      programs.nixvim = {
-        colorschemes.base16 = {
-          colorscheme = let
-            colors = config.lib.stylix.colors.withHashtag;
-          in {
-            base00 = colors.base00;
-            base01 = colors.base01;
-            base02 = colors.base02;
-            base03 = colors.base03;
-            base04 = colors.base04;
-            base05 = colors.base05;
-            base06 = colors.base06;
-            base07 = colors.base07;
-            base08 = colors.base08;
-            base09 = colors.base09;
-            base0A = colors.base0A;
-            base0B = colors.base0B;
-            base0C = colors.base0C;
-            base0D = colors.base0D;
-            base0E = colors.base0E;
-            base0F = colors.base0F;
-          };
-
-          enable = true;
-        };
-
-        highlight = let
-          cfg = config.stylix.targets.nixvim;
-          transparent = {
-            bg = "none";
-            ctermbg = "none";
-          };
+  config = lib.optionalAttrs (options.programs ? nixvim) (lib.mkIf config.stylix.targets.nixvim.enable {
+    programs.nixvim = {
+      colorschemes.base16 = {
+        colorscheme = let
+          colors = config.lib.stylix.colors.withHashtag;
         in {
-          Normal = lib.mkIf cfg.transparent_bg.main transparent;
-          NonText = lib.mkIf cfg.transparent_bg.main transparent;
-          SignColumn = lib.mkIf cfg.transparent_bg.sign_column transparent;
+          base00 = colors.base00;
+          base01 = colors.base01;
+          base02 = colors.base02;
+          base03 = colors.base03;
+          base04 = colors.base04;
+          base05 = colors.base05;
+          base06 = colors.base06;
+          base07 = colors.base07;
+          base08 = colors.base08;
+          base09 = colors.base09;
+          base0A = colors.base0A;
+          base0B = colors.base0B;
+          base0C = colors.base0C;
+          base0D = colors.base0D;
+          base0E = colors.base0E;
+          base0F = colors.base0F;
         };
+
+        enable = true;
       };
-    }
-  );
+
+      highlight = let
+        cfg = config.stylix.targets.nixvim;
+        transparent = {
+          bg = "none";
+          ctermbg = "none";
+        };
+      in {
+        Normal = lib.mkIf cfg.transparent_bg.main transparent;
+        NonText = lib.mkIf cfg.transparent_bg.main transparent;
+        SignColumn = lib.mkIf cfg.transparent_bg.sign_column transparent;
+      };
+    };
+  });
 }
